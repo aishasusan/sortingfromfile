240f1c991589ae923e3a8fcab47da617a22a3ab8
AISHA SUSAN GREGORY <aishasusan2@gmail.com>
2021-06-23 08:14:56 +0530

    last commit

--- src/main/java/com/user/SortFileDistinclty.java
+++ src/main/java/com/user/SortFileDistinclty.java
@@ -1,25 +1,31 @@
 package com.user;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class SortFileDistinclty {
 
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
 
 		// ArrayList string to hold the lines of input file
+		String x = "input.txt";
+		ArrayList<Integer> num = new ArrayList<Integer>();
+		 num = fileReading(x);
+		ArrayList<Integer> result = sort(num);
+		System.out.println("The result of operations are:\n" + result);
+	}
+
+	public static ArrayList<Integer> fileReading(String x) throws IOException {
+		// Creating BufferedReader to read the input file
 
 		String line;
 		ArrayList<Integer> numbers = new ArrayList<Integer>();
 
-		// Creating BufferedReader to read the input file
-
-		BufferedReader reader = new BufferedReader(new FileReader("input.txt"));
+		BufferedReader reader = new BufferedReader(new FileReader(x));
 
 		// Reading all the lines of input file one by one and adding them into ArrayList
 
@@ -36,17 +42,16 @@ public class SortFileDistinclty {
 				}
 			}
 		}
-		ArrayList<Integer> result = sort(numbers);
-		System.out.println("The result of operations are:\n"+result);
+		return numbers;
 	}
 
-	public static ArrayList<Integer>  sort(ArrayList<Integer> n) {
+	public static ArrayList<Integer> sort(ArrayList<Integer> n) {
 
 		System.out.println("Given input file: \n" + n);
 
-		ArrayList<Integer> sortedNumbers = new ArrayList<Integer>();
-		n.stream().sorted().distinct().forEach(s->sortedNumbers.add(s));    // to sort the input																							// of numbers
-		System.out.println("The result of operations are:\n"+sortedNumbers);
+		final ArrayList<Integer> sortedNumbers = new ArrayList<Integer>();
+		n.stream().sorted().distinct().forEach(s -> sortedNumbers.add(s)); // to sort the input of numbers
+		System.out.println("The result of operations are:\n" + sortedNumbers);
 		return sortedNumbers;
 
 	}
--- src/test/java/com/user/SortFileDistincltyTest.java
+++ src/test/java/com/user/SortFileDistincltyTest.java
@@ -1,31 +1,65 @@
 package com.user;
 
+import static org.junit.Assert.fail;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
 
-import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class SortFileDistincltyTest {
 
+
+//	@Test
+//	void test() {
+//		fail("Not Yet Implemented");
+//	}
+
+	
+	
 	private SortFileDistinclty obj = new SortFileDistinclty();
+	String line;
+	ArrayList<Integer> numbers = new ArrayList<Integer>();
+	ArrayList<Integer> n = new ArrayList<Integer>();
+	
+
+@Test
+public void inputFile() throws IOException {
+
+	BufferedReader bufReader = new BufferedReader(new FileReader("C:\\Users\\AISHA\\Desktop\\equifax\\SortingApp\\input.txt"));
+	n = obj.fileReading("input.txt");
+	Assertions.assertEquals(n,obj.fileReading("input.txt"));
+	//System.out.println(obj.fileReading("input.txt")); 
+}	
 
 	@Test
-	public void finalResult() {
+	public void finalResult() throws IOException {
 
-		//to save the outputs 
-		List<Integer> outputList = Arrays.asList(1, 2, 3, 4, 6, 8, 9, 12, 22, 42, 45, 50, 66, 88, 100);
-		ArrayList<Integer> output = new ArrayList<Integer>();
-		output.addAll(outputList);
-
-		//to save the inputs
-		List<Integer> inputList = Arrays.asList(6, 8, 1, 1, 9, 9, 22, 12, 45, 42, 45, 88, 100, 88, 66, 50, 4, 2, 3);
-		ArrayList<Integer> input = new ArrayList<Integer>();
-		input.addAll(inputList);
-
-		Assertions.assertEquals(output, obj.sort(input));
+//		// to save the inputs
+//		List<Integer> inputList = Arrays.asList(6, 8, 1, 1, 9, 9, 22, 12, 45, 42, 45, 88, 100, 88, 66, 50, 4, 2, 3);
+//		ArrayList<Integer> input = new ArrayList<Integer>();
+//		input.addAll(inputList);
+ 
+		numbers = obj.fileReading("input.txt");
 		
+		final ArrayList<Integer> sortedNumbers = new ArrayList<Integer>();
+		
+		numbers.stream().sorted().distinct().forEach(s -> sortedNumbers.add(s));
+		 
+		//System.out.println(numbers);
+		
+		Assertions.assertEquals(sortedNumbers, obj.sort(numbers));
+
 	}
 
 }
